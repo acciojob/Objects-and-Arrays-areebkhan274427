77be570924b6fa450025f74c617b7ed91497376d
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-03-21 00:59:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,5 +7,8 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
+let team=players;
+const team1 = Array.from(players);
+let cap1 = JSON.parse(JSON.stringify(person));
 
 export {players,person,team,team1,cap1}
